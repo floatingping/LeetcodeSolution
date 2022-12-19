c823e6a0b5650b4c54ec9d238ece5609de565d8e
floatingping <floatingping@gmail.com>
2022-12-20 00:39:38 +0800

    feat: add QA<T,U>

---
+++ Utils/QA.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeSolution.Utils
+{
+    public class QA<T, U>
+        where T : class
+        where U : class
+    {
+        public T Input { get; set; }
+        public U Output { get; set; }
+    }
+}
