d5777516c4fa257db98d86422304a219ee32d489
floatingping <floatingping@gmail.com>
2023-03-04 16:35:05 +0800

    refactor: BestTimetoBuyandSellStock for leetcode code template

--- Solutions/BestTimetoBuyandSellStock.cs
+++ Solutions/BestTimetoBuyandSellStock.cs
@@ -1,3 +1,4 @@
+using LeetcodeSolution.Utils;
 using System;
 using System.Linq;
 using Xunit;
@@ -5,62 +6,71 @@ using Xunit;
 namespace LeetcodeSolution
 {
     public class BestTimetoBuyandSellStock
-
     {
         [Theory]
-        [InlineData(5, 7, 1, 5, 3, 6, 4)]
-        [InlineData(0, 7, 6, 4, 3, 1)]
-        [InlineData(4, 3, 2, 6, 5, 0, 3)]
-        public void Test(int expect, params int[] vals)
+        [InlineData("[7, 1, 5, 3, 6, 4]", 5)]
+        [InlineData("[7, 6, 4, 3, 1]", 0)]
+        [InlineData("[3, 2, 6, 5, 0, 3]", 4)]
+        public void Test(string input, int expect)
         {
-            int actual = MaxProfit(vals);
+            var prices = LeetcodeUtils.ConvertToObject<int[]>(input);
+            var aSolution = new Solution();
+
+            var actual = aSolution.MaxProfit(prices);
 
             Assert.Equal(expect, actual);
         }
 
-        public int MaxProfit(int[] prices)
+
+        public class Solution
         {
-            if (prices.Count() == 1) return 0;
-            var aMaxProfitRealizer = new MaxProfitRealizer(prices[0]);
-
-            for(int i = 1; i< prices.Count(); i++)
+            public int MaxProfit(int[] prices)
             {
-                aMaxProfitRealizer.Read(prices[i]);
-            }
-            return aMaxProfitRealizer.MaxProfit;
-        }
+                if (prices.Count() == 1) return 0;
+                var aMaxProfitRealizer = new MaxProfitRealizer(prices[0]);
 
-        private class MaxProfitRealizer
-        {
-            private int _lowestPrice;
-            private int _higestPrice;
-            public int MaxProfit { get; private set; }
-            public MaxProfitRealizer(int initPrice)
-            {
-                _lowestPrice = initPrice;
-                _higestPrice = initPrice;
-                MaxProfit = 0;
-            }
-
-
-            public void Read(int price)
-            {
-                if (price > _higestPrice)
+                for (int i = 1; i < prices.Count(); i++)
                 {
-                    _higestPrice = price;
-                    int profix = price - _lowestPrice;
-                    MaxProfit = profix > MaxProfit
-                        ? profix
-                        : MaxProfit;
+                    aMaxProfitRealizer.Read(prices[i]);
+                }
+                return aMaxProfitRealizer.MaxProfit;
+            }
+
+            private class MaxProfitRealizer
+            {
+                private int _lowestPrice;
+                private int _higestPrice;
+                public int MaxProfit { get; private set; }
+                public MaxProfitRealizer(int initPrice)
+                {
+                    _lowestPrice = initPrice;
+                    _higestPrice = initPrice;
+                    MaxProfit = 0;
                 }
 
-                if (price < _lowestPrice)
-                {
-                    _lowestPrice = price;
-                    _higestPrice = price;
-                }
-            }
 
+                public void Read(int price)
+                {
+                    if (price > _higestPrice)
+                    {
+                        _higestPrice = price;
+                        int profix = price - _lowestPrice;
+                        MaxProfit = profix > MaxProfit
+                            ? profix
+                            : MaxProfit;
+                    }
+
+                    if (price < _lowestPrice)
+                    {
+                        _lowestPrice = price;
+                        _higestPrice = price;
+                    }
+                }
+
+            }
         }
+
+
+
     }
 }
